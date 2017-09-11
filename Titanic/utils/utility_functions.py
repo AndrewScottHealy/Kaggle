fb99aa83ea7a0a2da54946c4be4e042a80990d2d
Andrew Healy <andrew.scott.healy@gmail.com>
2017-09-10 21:48:33 -0600

    ARGH

--- Titanic/utils/utility_functions.py
+++ Titanic/utils/utility_functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
-#import numpy as np
-#import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
+trainData = pd.read_csv("Titanic/data/train.csv")
 
-#trainData = pd.read_csv("../data/train.csv")
